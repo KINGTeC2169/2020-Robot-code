3a60698706949e75f8fab35eb245476d9a11e85f
Pizzascripters <pizzascripters@gmail.com>
2020-01-08 14:27:05 -0600

    Color sensor

--- src/main/java/com/kingtec/frc2020/Robot.java
+++ src/main/java/com/kingtec/frc2020/Robot.java
@@ -1,16 +1,22 @@
 package com.kingtec.frc2020;
 
 import com.kingtec.frc2020.auto.actions.DoNothing;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+
+import com.revrobotics.ColorSensorV3;
 
 public class Robot extends TimedRobot {
 
-    Command autoCommand;
-    SendableChooser<Command> chooser = new SendableChooser<>();
+    private ColorSensorV3 colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
+
+    private Command autoCommand;
+    private SendableChooser<Command> chooser = new SendableChooser<>();
 
     @Override
     public void robotInit() {
@@ -21,6 +27,20 @@ public class Robot extends TimedRobot {
     @Override
     public void disabledInit(){
 
+    }
+
+    @Override
+    public void robotPeriodic() {
+        Color detected = colorSensor.getColor();
+        double IR = colorSensor.getIR();
+
+        SmartDashboard.putNumber("Red", detected.red);
+        SmartDashboard.putNumber("Green", detected.green);
+        SmartDashboard.putNumber("Blue", detected.blue);
+        SmartDashboard.putNumber("IR", IR);
+
+        int proximity = colorSensor.getProximity();
+        SmartDashboard.putNumber("Proximity", proximity);
     }
 
     @Override
