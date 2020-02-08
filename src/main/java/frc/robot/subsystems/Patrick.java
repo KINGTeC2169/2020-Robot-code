7e2155d55ba5c0c7edf6a4c7467310611d53841e
Ethan Fontana <ethan@kinura.net>
2020-02-08 15:50:15 -0600

    Patrick subsystem

--- src/main/java/frc/robot/subsystems/Patrick.java
+++ src/main/java/frc/robot/subsystems/Patrick.java
@@ -1,6 +1,12 @@
 package frc.robot.subsystems;
 
+import frc.util.ActuatorMap;
+import frc.util.Controls;
 import frc.util.drivers.ColorSensor;
+import frc.util.drivers.ControllerFactory;
+import frc.util.drivers.Talon;
+
+import java.lang.reflect.Array;
 
 public class Patrick implements Subsystem {
     private static Patrick instance;
@@ -13,14 +19,75 @@ public class Patrick implements Subsystem {
     }
 
     ColorSensor colorSensor;
+    private Talon wheelSpin = ControllerFactory.masterTalon(ActuatorMap.patrick, false);
+    private boolean wheelState = false;
+    private boolean colorFound = false;
+    private Controls controls;
+    private int slices;
+    private ColorSensor.CpColor lastCol;
+    private final ColorSensor.CpColor colorVal [] = {ColorSensor.CpColor.CYAN, ColorSensor.CpColor.YELLOW, ColorSensor.CpColor.GREEN, ColorSensor.CpColor.RED};
 
-    public Patrick() {
+    public Patrick ()
+    {
         colorSensor = ColorSensor.getInstance();
+        controls = Controls.getInstance();
+    }
+
+    public boolean getWheelSpin() {
+       if (wheelSpin.getOutput() > .5) {
+           wheelState = true;
+       }
+        return wheelState;
+    }
+
+    public ColorSensor getColorSensor() {
+        if ()
+        {
+            colorFound = true;
+        }
+        return colorSensor;
+    }
+
+    public void rotationalControl(ColorSensor.CpColor detectedColor) {
+        for (int i = 0; i < colorVal.length; i++) {
+
+            if (detectedColor == colorVal[i]) {
+                break;
+            }
+        }
+
+        if (slices  >= 25) {
+            wheelSpin.setOutput(1);
+        }
+        else {
+            wheelSpin.setOutput(0);
+        }
+    }
+
+    public void positionalControl() {
+
+
+    }
+
+
+
+
+    public Talon setWheel() {
+        if(colorFound && wheelState) {
+            wheelState = false;
+            Talon wheelSpin
+        }
     }
 
     @Override
     public void update() {
         ColorSensor.CpColor detectedColor = colorSensor.getColor();
+        if(controls.xbox.getXButtonPressed())
+        {
+            rotationalControl();
+        }
+
+        detectedColor = lastCol;
     }
 
     @Override
